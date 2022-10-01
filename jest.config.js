49b129a65ae8f860de67011ee9f4035ec251ae13
Archie Lamb <git@archielamb.uk>
2022-10-01 20:32:34 +0100

    Fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  clearMocks: true,
-  moduleFileExtensions: ['js', 'ts'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest'
-  },
-  verbose: true
 }
