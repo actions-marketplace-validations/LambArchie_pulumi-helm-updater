457652f04112477e9fcedf0082a21af50ee3162a
Archie Lamb <git@archielamb.uk>
2022-10-02 21:58:07 +0100

    Fix lint

--- example/index.ts
+++ example/index.ts
@@ -1,11 +1,11 @@
-import * as pulumi from '@pulumi/pulumi'
 import * as k8s from '@pulumi/kubernetes'
 import * as helmVersions from './helmVersions'
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
 const argoCdApplicationset = new k8s.helm.v3.Release('argo-cd-applicationset', {
   name: 'argocd-applicationset',
   chart: 'argocd-applicationset', // Purposefully chose deprecated chart so never any updates available
-  version: helmVersions.argocdApplicationset,
+  version: helmVersions.argocdApplicationsetVersion,
   repositoryOpts: {
     repo: 'https://argoproj.github.io/argo-helm',
   },
@@ -24,7 +24,7 @@ const argoCdApplicationset = new k8s.helm.v3.Release('argo-cd-applicationset', {
 const argoCdNotifications = new k8s.helm.v3.Release('argo-cd-notifications', {
   name: 'argocd-notifications',
   chart: 'argocd-notifications', // Purposefully chose deprecated chart so never any updates available
-  version: helmVersions.argocdNotifications, // Chose one update behind latest
+  version: helmVersions.argocdNotificationsVersion, // Chose one update behind latest
   repositoryOpts: {
     repo: 'https://argoproj.github.io/argo-helm',
   },
