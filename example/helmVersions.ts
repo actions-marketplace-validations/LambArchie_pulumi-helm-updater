5f8674c021e6e3902678376147f3c31c47c65bcb
Archie Lamb <git@archielamb.uk>
2022-10-02 23:06:00 +0100

    Add CI check to ensure test file hasn't been modified before running

--- example/helmVersions.ts
+++ example/helmVersions.ts
@@ -1,5 +1,5 @@
 // This file still supports comments
 export const argocdApplicationsetVersion = '1.12.1' // Even at the end of the line
-export const argocdNotificationsVersion = '1.8.1' // Purposfully out of date
+export const argocdNotificationsVersion = '1.8.0' // Purposfully out of date
 export const nonExistantInStackVersion = '1.2.3'
 export const unrelatedVariable = 'abc'
